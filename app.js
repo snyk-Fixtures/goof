52a3d4dcc1940b441ac4eadeeb60c6ad59c8bb0d
Patricia Vale <patricia.vale@snyk.io>
2022-02-23 13:05:34 +0100

    fix: http

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ require('./db');
 var st             = require('st');
 var crypto         = require('crypto');
 var express        = require('express');
-var http           = require('http');
+var https          = require('http');
 var path           = require('path');
 var engine         = require('ejs-locals');
 var cookieParser   = require('cookie-parser');
